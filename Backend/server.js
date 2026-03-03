a25dff7fd44d1ba741c160eb3a9de241cf68f7aa
rinashewale111 <rinashewale6@gmail.com>
2026-03-03 19:10:42 +0530

    backend-frontend

--- Backend/server.js
+++ Backend/server.js
@@ -3,22 +3,20 @@ const express = require("express");
 const path = require("path");
 const connectToDb = require("./src/config/database");
 
-// Import backend app
 const app = require("./src/app");
 
-// Connect to MongoDB
 connectToDb();
 
-// Serve frontend (Vite build folder)
+// Serve React build folder
 app.use(express.static(path.join(__dirname, "Frontend/dist")));
 
 // React Router fallback
-app.use((req, res) => {
+app.get("*name", (req, res) => {
   res.sendFile(path.join(__dirname, "Frontend/dist/index.html"));
 });
 
-// Dynamic port for Render or local
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});  
+  console.log(`Server is running on port ${PORT}`);
+});
