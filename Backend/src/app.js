cfd967dcef88bf9bcc12ea72258c7d0ebebc3116
rinashewale111 <rinashewale6@gmail.com>
2026-03-03 14:59:42 +0530

    backend+frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ const cors = require("cors")
 const app = express()
 
 app.use(cors({
-    origin: "https://instagram-clone-five-teal.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true
 }))
 
--- Frontend/src/features/Posts/Services/followeapi.js
+++ Frontend/src/features/Posts/Services/followeapi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://instagram-clone-1-s69s.onrender.com/api",
     withCredentials: true
 })
 
