e79acf8ab028b0cfe4d459d7f6c2544c14d50acf
rinashewale111 <rinashewale6@gmail.com>
2026-03-03 16:08:04 +0530

    backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,13 @@
 require("dotenv").config()
-const app=require("./src/app")
-const connectToDb=require("./src/config/database")
+const app = require("./src/app")
+const connectToDb = require("./src/config/database")
 
 connectToDb()
+app.get("/", (req, res) => {
+    res.send("Server is running ");
+});
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("server is running on port 3000");
-    
+
 })
